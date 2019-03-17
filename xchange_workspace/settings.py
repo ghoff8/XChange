a11974405d7afa009a134d109090b72cd2c27365
Grant Hoffmann <ghoffma3@uncc.edu>
2019-03-17 22:22:02 +0000

    CSS configured

--- xchange_workspace/settings.py
+++ xchange_workspace/settings.py
@@ -121,4 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'XChange/static')
+
+STATIC_URL = '/static/XChange/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "XChange/static/XChange"),
+]
--- xchange_workspace/urls.py
+++ xchange_workspace/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('XChange.urls'), name='index'),
