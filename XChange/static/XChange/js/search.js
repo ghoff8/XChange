1e078571e0e9d30888ee0ca01b941e539d0cdf1f
brownshugga95 <tchacko@uncc.edu>
2019-04-06 23:06:21 +0000

    search.js comment out

---
+++ XChange/static/XChange/js/search.js
@@ -0,0 +1,5 @@
+// window.onload = function() {
+//     if (!(document.getElementById("currGrowth").innerHTML).includes("-")) {
+//         document.getElementById("currGrowth").innerHTML.style.color = "green";
+//     }
+// }
