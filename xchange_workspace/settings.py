816e36934e6c9434eb299645aa4132f1c90fc571
ghoffma3 <ghoffma3@uncc.edu>
2019-04-28 20:02:42 +0000

    requirements

--- xchange_workspace/settings.py
+++ xchange_workspace/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '81#_=k$^6r!xxo&9-_ukb443q4j#442+*m5=3rxe4p$f2zs8s7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fac8c70386894602883d2fedb9d94fa4.vfs.cloud9.us-east-2.amazonaws.com', '21bc43a5bf7c407483a88d663e63adce.vfs.cloud9.us-east-2.amazonaws.com']
-
+#ALLOWED_HOSTS = ['fac8c70386894602883d2fedb9d94fa4.vfs.cloud9.us-east-2.amazonaws.com', '21bc43a5bf7c407483a88d663e63adce.vfs.cloud9.us-east-2.amazonaws.com']
+ALLLOWED_HOSTS = [*]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
